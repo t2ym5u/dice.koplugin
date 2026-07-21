d6b5679120a057a9133c08654ad81bf84fbd75c8
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 15:47:03 +0200

    chore: bump version to 1.0.2

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "dice",
     fullname    = _("Dice"),
     description = _("Roll virtual dice with a configurable number of faces and dice."),
-    version     = "1.0.1",
+    version     = "1.0.2",
 }
