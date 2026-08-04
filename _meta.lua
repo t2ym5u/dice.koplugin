041ffc0f4ba5f8c0773544cf55324dabb5d6b0da
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:50:10 +0200

    chore: bump version to 1.0.9

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Dice"),
     description = _("Roll virtual dice with a configurable number of faces and dice."),
-    version     = "1.0.8",
+    version     = "1.0.9",
 }
